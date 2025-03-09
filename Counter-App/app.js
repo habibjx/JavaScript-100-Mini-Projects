06ef1f7a0884dcbceeba98cb51361d6c272183b8
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-03-09 12:05:19 +0600

    counter app complate

--- Counter-App/app.js
+++ Counter-App/app.js
@@ -1,15 +1,15 @@
 
 
 /**
- * Date : Feb 08 2025
- * Title : Counter App
+ * Date : Feb 08 2025.
+ * Title : Counter App.
  * Description : This is a simple counter app where users can easily increase or decrease the number.
  * Author : M H R Habib.
  */
 
 
 // Globals variable
-
+const reset = document.querySelector('.reset');
 
 let counter = Number(localStorage.getItem('countNum')) || 0;
 
@@ -19,51 +19,68 @@ let counter = Number(localStorage.getItem('countNum')) || 0;
 
 window.onload = () => {
 
-    main()
-    updateCounterValue()
+    main();
+    updateCounterValue();
 }
 
 function main(){
     // DOM reference 
     const decrease = document.querySelector('.decrease');
     const increase = document.querySelector('.increase');
-    const reset = document.querySelector('.reset');
-
+    
 
     // Event Listener
-    decrease.addEventListener('click', () => {
-        counter --;
-        updateCounterValue();
-        displayCounterValue();
-        reset.classList.remove('disable');
-    });
-    increase.addEventListener('click', () => {
-        counter ++;
-        updateCounterValue();
-        displayCounterValue();
-        reset.classList.remove('disable');
-    })
-    reset.addEventListener('click', () => {
-        counter = 0;
-        displayCounterValue()
-        updateCounterValue();
-        reset.classList.add('disable');
-    })
-}
+    decrease.addEventListener('click', decreaseHandler);
+    increase.addEventListener('click', increaseHandler) ;
+    reset.addEventListener('click', resetHandler);
 
+    //Keyboard Listener
+    document.addEventListener('keydown', (event) => {
+        if(event.key === 'ArrowRight'){
+            increaseHandler();
+        }else if(event.key === 'ArrowLeft'){
+            decreaseHandler();
+        }
+        else if(event.key === ' '){
+            resetHandler();
+        }
+    })
+};
 
 
 //Event handler ====================
 
+function decreaseHandler(){
+    counter --;
+    if(counter < -5) counter = 0;
+    updateCounterValue();
+};
+
+function increaseHandler(){
+    counter ++;
+    if(counter > 5) counter = 0;
+    updateCounterValue();
+};
+
+function resetHandler(){
+    counter = 0;
+    updateCounterValue();
+};
+
 //DOM function ====================
 
 function updateCounterValue(){
+    updateLocalStorage();
+    resetButtonStatus();
     document.querySelector('.display').textContent = counter; 
-}
+};
 
+function resetButtonStatus(){
+    reset.classList.toggle('disable', counter === 0);
+};
 
 //Utils function ====================
 
-function displayCounterValue(){
+function updateLocalStorage(){
     localStorage.setItem('countNum', counter);
-}
+};
