859320b1779ffebb74b51cf020fe2e5be96f803d
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-01 22:33:34 +0600

    digital clock js repo code optimize

--- Digita-Clock/app.js
+++ Digita-Clock/app.js
@@ -3,10 +3,27 @@
  * description: This JavaScript-powered digital clock updates every second, supports both 12-hour and 24-hour formats, and displays the current date.
  * Author: M H R Habib.
  */
-//Global Variable
 
+//Global Variable
 // let timeSessionHalf = JSON.parse(localStorage.getItem('sessionTime')) || true;
 let timeSessionHalf = JSON.parse(localStorage.getItem('sessionTime') ?? 'true');
+const seconds = document.getElementById('seconds');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const days = document.getElementById('days');
+const months = document.getElementById('months');
+const years = document.getElementById('years');
+const amPm = document.getElementById('ampm');
+const timeMood = document.querySelector('.time-mood');
+const timeMood2 = document.getElementById('timeMood');
+const timeDivider = document.getElementById('timeDivider');
+
+const dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthList = [
+    "January", "February", "March", "April", "May", "June", 
+    "July", "August", "September", "October", "November", "December"
+];
+
 
 window.onload = () =>{
     main()
@@ -27,28 +44,24 @@ function timeUpdate(){
     localStorage.setItem('sessionTime', JSON.stringify(timeSessionHalf)); 
     switchOnOff()
 
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const months = [
-        "January", "February", "March", "April", "May", "June", 
-        "July", "August", "September", "October", "November", "December"
-    ];
-    const {hours, minutes, seconds, day, month,year} = getTimeAndDate();
+    const {hrs, mins, snd, dys, mons, yrs} = getTimeAndDate();
     if(timeSessionHalf){
         const {hour, session} = halfSessionTime();
-        document.getElementById('hours').textContent = String(hour).padStart(2, '0');
-        document.getElementById('ampm').textContent = session;
-        document.querySelector('.time-mood').style.display = 'grid';
-        document.getElementById('timeMood').textContent = '12H';
-    }else{
-        document.getElementById('hours').textContent = hours;
-        document.querySelector('.time-mood').style.display = 'none';
-        document.getElementById('timeMood').textContent = '24H';
+        hours.textContent = String(hour).padStart(2, '0');
+        amPm.textContent = session;
+        timeMood.style.display = 'grid';
+        timeMood2.textContent ='12H';
     }
-    document.getElementById('minutes').textContent = minutes;
-    document.getElementById('seconds').textContent = seconds;
-    document.getElementById('days').textContent =  days[day];
-    document.getElementById('months').textContent = months[month];
-    document.getElementById('years').textContent = year;
+    else{
+        hours.textContent = hrs;
+        timeMood.style.display = 'none';
+        timeMood2.textContent = '24H';
+    }
+    minutes.textContent = mins;
+    seconds.textContent = snd;
+    days.textContent = dayList[dys];
+    months.textContent = monthList[mons];
+    years.textContent = yrs;
 }
 
 function handleSwitchButton(){
@@ -60,18 +73,19 @@ function handleSwitchButton(){
 
 function getTimeAndDate(){
     const time = new Date();
-    const hours = String(time.getHours()).padStart(2, '0');
-    const minutes = String(time.getMinutes()).padStart(2, '0');
-    const seconds = String(time.getSeconds()).padStart(2, '0');
-    const day = time.getDay();
-    const month = time.getMonth();
-    const year = time.getFullYear();
-    return {hours, minutes, seconds, day, month,year};
+    return {
+        hrs : String(time.getHours()).padStart(2, '0'),
+        mins : String(time.getMinutes()).padStart(2, '0'),
+        snd : String(time.getSeconds()).padStart(2, '0'),
+        dys : time.getDay(),
+        mons : time.getMonth(),
+        yrs : time.getFullYear()
+    };
 }
 
 function halfSessionTime(){
-    const {hours} = getTimeAndDate();
-    let hour = Number(hours)
+    const { hrs } = getTimeAndDate();
+    let hour = Number(hrs)
     let session = 'AM';
     if(hour >= 12){
         session = 'PM';
