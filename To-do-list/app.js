4ef6b6fa01047a68dfdf73e22e1e2798d317f647
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-02 21:39:35 +0600

    todolist: add js for node add and delete

---
+++ To-do-list/app.js
@@ -0,0 +1,69 @@
+/**
+ * 02/04/2025.
+ * Description:
+ * Author: M H R Habib.
+ */
+
+// Global Variables
+
+window.onload = () => {
+    main()
+
+}
+
+function main(){
+    // DOM References =========
+    const userInput = document.getElementById('userInput');
+    const addNodeButton = document.getElementById('addNodeButton');
+    const nodeListsParent = document.getElementById('nodeListsParent');
+
+
+    // Event Listener ===========
+    addNodeButton.addEventListener('click', () => addNodeButtonHandle(userInput, nodeListsParent));
+
+    nodeListsParent.addEventListener('click', (event) => nodeListsParentHandle(event));
+}
+
+// Event Handler ==========
+function addNodeButtonHandle(userInput, nodeListsParent){
+    const node = userInput.value;
+    if(node === ''){
+        alert('Please Enter Node');
+        return;
+    }
+    nodeListUpdate(nodeListsParent, node)
+    userInput.value = '';
+}
+
+// DOM function ==============================
+
+function nodeListUpdate(element, node){
+    const list = document.createElement('div');
+    const checkMark = document.createElement('div');
+    const p = document.createElement('p');
+    const deleteBox = document.createElement('div');
+    const i = document.createElement('i');
+
+    list.classList.add('list')
+    checkMark.classList.add('check-mark');
+    p.innerText = node;
+    deleteBox.classList.add('delete-box');
+    i.classList.add('fa-solid', 'fa-trash-can');
+
+    deleteBox.appendChild(i);
+    list.append(checkMark, p, deleteBox);
+    element.appendChild(list);
+}
+
+function nodeListsParentHandle(event){
+    const deleteButton = event.target.closest('.delete-box');
+    if(deleteButton){
+        const parent = deleteButton.parentElement
+        if(parent){
+            parent.remove();
+        }
+    }
+}
+
+
+//Utilities Function ========================
