e27003f2766f07407fe6c18f2dd74698ad82cb47
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-10 18:07:24 +0600

    rgb hex color generate complate & dom display

--- random-color-generator/app.js
+++ random-color-generator/app.js
@@ -14,20 +14,43 @@ function main(){
     //DOM References
     const randomButton = document.getElementById("randomButton");
     const rgbBox = document.getElementById("rgbBox");
-    const hslBox = document.getElementById("hslBox");
     const hexBox = document.getElementById("hexBox");
     const colorDisplay = document.getElementById("colorDisplay");
 
     //Event Listener
-    randomButton.addEventListener("click", handleRandomButton);
+    randomButton.addEventListener("click", () => handleRandomButton(rgbBox, hexBox, colorDisplay));
 
 }
 
 //Event handler
-function handleRandomButton(){
-    console.log("ok random")
+function handleRandomButton(rgbBox, hexBox, colorDisplay){
+    DOMColorUpdate(rgbBox, hexBox, colorDisplay);
+    
 }
 
 //DOM function
+function DOMColorUpdate(rgbBox, hexBox, colorDisplay){
+    const {r, g, b} = generateRGBColor();
+    const {rh, gh, bh} = rgbToHexColor({r, g, b});
+    rgbBox.value = `${r}, ${g}, ${b}`;
+    hexBox.value = `${rh}${gh}${bh}`;
+    const rgbColor = `rgb(${r}, ${g}, ${b})`;
+    colorDisplay.style.background = rgbColor;
+}
 
 //Utilities Function
+
+function generateRGBColor(){
+    const r = Math.floor(Math.random() *  255);
+    const g = Math.floor(Math.random() *  255);
+    const b = Math.floor(Math.random() *  255);
+    return{r, g, b};
+}
+
+function rgbToHexColor(rgb){
+    const {r, g, b} = rgb;
+    let rh = r.toString(16).padStart(2, "0");
+    let gh = g.toString(16).padStart(2, "0");
+    let bh = b.toString(16).padStart(2, "0");
+    return {rh, gh, bh};
+}
