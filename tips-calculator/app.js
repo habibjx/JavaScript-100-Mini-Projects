837b255a108100df22123dbec3b0adf67f870d4f
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-18 14:03:53 +0600

    style redesing and dom referece complate

---
+++ tips-calculator/app.js
@@ -0,0 +1,36 @@
+/**
+ * Date: 18-04-2025.
+ * Description: 
+ * Author M H R Habib.
+ */
+
+// Global Variables
+
+window.onload = function(){
+    main();
+
+}
+
+function main(){
+    //DOM Reference
+    const billAmount = document.getElementById("billAmount");
+    const numOfPeoples = document.getElementById("numOfPeoples");
+    const tipsPercentage = document.getElementById("tipsPercentage");
+    const preValuePercent1 = document.getElementById("preValuePercent1");
+    const preValuePercent2 = document.getElementById("preValuePercent2");
+    const preValuePercent3 = document.getElementById("preValuePercent3");
+    const AmountPerPerson = document.getElementById("AmountPerPerson");
+    const tipsAmount = document.getElementById("tipsAmount");
+    const totalAmount = document.getElementById("totalAmount");
+    const resetBtn = document.getElementById("resetBtn");
+
+
+    //Event Listener
+
+}
+
+// Event Handler ==================
+
+//DOM function ==================
+
+//Utilities function ==================
