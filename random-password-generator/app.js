3d6a7bb0aacd4d44390a1561f17a35505902de8a
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-22 12:18:44 +0600

    add & update: range value display container add & password generator function add

--- random-password-generator/app.js
+++ random-password-generator/app.js
@@ -5,10 +5,15 @@
  */
 
 // Global Variable
+let upperCase = "true";
+let lowerCase = "true";
+let numbers = "true";
+let symbols = "true";
 
 window.onload = () => {
     main();
 
+    passwordGenerator();
 }
 
 function main(){
@@ -21,6 +26,7 @@ function main(){
     const password = document.getElementById("password");
     const copy = document.getElementById("copy");
     const copyText = document.getElementById("copyText");
+    const reGeneratePass = document.getElementById("reGeneratePass");
 
     // Event Listener
     passwordRange.addEventListener("input", (e) => updateInputRange(e.target.value, selector));
@@ -39,25 +45,33 @@ function handlePassTypeSwitch(element){
     const identity = element.dataset.identity;
     element.addEventListener("click", () => {
         if(identity === "ABC"){
-            passTypeSwitch(element)
+            passTypeSwitch(element);
+            upperCase = passCharTrueFalse(element);
+            passwordGenerator();
         }
         else if(identity === "abc"){
-            passTypeSwitch(element)
+            passTypeSwitch(element);
+            lowerCase = passCharTrueFalse(element);
+            passwordGenerator();
         }
         else if(identity === "123"){
-            passTypeSwitch(element)
+            passTypeSwitch(element);
+            numbers = passCharTrueFalse(element);
+            passwordGenerator();
         }
         else if(identity === "$#&"){
-            passTypeSwitch(element)
+            passTypeSwitch(element);
+            symbols = passCharTrueFalse(element);
+            passwordGenerator();
         }
     })
 }
+
 
 function handleIncreaseDecrease(range, selector, inDe){
     range.value = Number(range.value) + (inDe);
     let value = Number(range.value);
     updateInputRange(value, selector);
-    console.log(value)
 }
 
 function handleCopy(input, copyText){
@@ -78,17 +92,26 @@ function handleCopy(input, copyText){
     }
 }
 
+
 // DOM function
 
 function passTypeSwitch(element){
+    
     element.classList.toggle("passType-bg")
     const switchPoint = element.querySelector(".pass-switch-point");
     if(element.classList.contains("passType-bg")){
         switchPoint.style.left = `calc(100% - 15px)`;
+        element.dataset.on = "true";
     }
     else{
         switchPoint.style.left = "3px";
+        element.dataset.on = "false";
     }
+    
+}
+
+function  passTypeSwitchOnOff(element){
+    console.log(element)
 }
 
 /**
@@ -97,6 +120,7 @@ function passTypeSwitch(element){
  * @param {HTMLCollection} selector 
  */
 function updateInputRange(value, selector){
+    document.getElementById("rangeValueDisplay").textContent = value;
     const parent = selector.parentElement;
     const parentWidth = parent.offsetWidth;
     const childWidth = selector.offsetWidth;
@@ -110,4 +134,31 @@ function updateInputRange(value, selector){
 }
 
 // Utilities function
+
+function passCharTrueFalse(element){
+    return element.dataset.on;
+}
+
+function passwordGenerator(length){
+    let passLength = length  || 8;
+    const passInput = document.getElementById("password");
+    const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    const DecimalNumbers = "0123456789";
+    const symbol = "!@#$%^&*()_+[]{}|;:,.<>?";
+    const password = [];
+    
+    if(upperCase === "true") password.push(...upperCaseLetters);   
+    if(lowerCase === "true") password.push(...lowerCaseLetters);
+    if(numbers === "true") password.push(...DecimalNumbers);
+    if(symbols === "true") password.push(...symbol);
+    passInput.value = "";
+
+    if(password && password.length >= 8){
+        for(let i = 0; i < passLength; i++){
+            const random = Math.floor(Math.random() * password.length);
+            passInput.value += password[random];
+        }
+    }
+}
 
