c21e3ed20235674ce9dbacca647a1842006ab85f
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-04-25 22:30:29 +0600

    add: dom references complete

--- weather-application/app.js
+++ weather-application/app.js
@@ -3,17 +3,12 @@
  * Description:
  * Author: M H R Habib.
  */
+
 // Global Variables
-const days = [
-    "Sunday", "Monday", "Tuesday", 
-    "Wednesday", "Thursday", "Friday", 
-    "Saturday"
-];
-const months = [
-    "Jan", "Feb", "Mar", "Apr", 
-    "May", "Jun", "Jul", "Aug", 
-    "Sep", "Oct", "Nov", "Dec"
-];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let cityName = "Dhaka";
 
 window.onload = () => {
     main();
@@ -27,6 +22,15 @@ function main(){
     const search = document.getElementById("search");
     const searchButton = document.getElementById("searchButton");
     const temperature = document.getElementById("temperature");
+    const tempFeel = document.getElementById("tempFeel");
+    const weatherCondition = document.getElementById("weatherCondition");
+    const windText = document.getElementById("windText");
+    const windSpeed = document.getElementById("windSpeed");
+    const humidityText = document.getElementById("humidityText");
+    const humidityCondition = document.getElementById("humidityCondition");
+    const cloudText = document.getElementById("cloudText");
+    const cloudCondition = document.getElementById("cloudCondition");
+    console.log(temperature, tempFeel, weatherCondition, windText, windSpeed, humidityText, humidityCondition, cloudText, cloudCondition);
 
     // Event Listener
     searchButton.addEventListener("click", () => {
