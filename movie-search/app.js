dfe0f69583966a6e80872babda6f81ace85d9919
Md Habibur Rahman Habib <mhrhabib77232@gmail.com>
2025-05-05 12:37:27 +0600

    add: html complete

---
+++ movie-search/app.js
@@ -0,0 +1,7 @@
+
+// async function search() {
+//     const res = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=6af6e47a");
+//     const data = await res.json()
+//     console.log(data)
+// }
+// search()
